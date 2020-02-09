0883c25b3f0c1c61e6ec8b130db3d1a725c93f29
297jy <1347704262@qq.com>
2020-02-09 23:10:03 +0800

    修复权限管理出现的问题，并添加检查邮箱是否唯一

--- flowmeter/middlewares/auth.py
+++ flowmeter/middlewares/auth.py
@@ -14,15 +14,29 @@ class AuthMiddleware(MiddlewareMixin):
     """
     权限检查中间件，如果没有相应的权限就拦截
     """
-
     @staticmethod
-    def __is_path_white_list(path):
+    def __not_check_login(path):
         """
-        检查path是否在不用检查权限的白名单中
+        不用检查登录的路径
         :param path:
         :return:
         """
-        path_white_list = ['/login/', '/handler/login/', '/index/', '/welcome/', '/error/']
+        no_check_list = ['/login/', '/handler/login/', '/error/']
+
+        for white in no_check_list:
+            if re.match(white, path):
+                return True
+
+        return False
+
+    @staticmethod
+    def __not_check_auth(path):
+        """
+        不用检查权限的路径
+        :param path:
+        :return:
+        """
+        path_white_list = ['/index/', '/welcome/', '/error/']
 
         for white in path_white_list:
             if re.match(white, path):
@@ -33,16 +47,20 @@ class AuthMiddleware(MiddlewareMixin):
     def process_request(self, request):
 
         path = request_api.get_path(request)
-        if AuthMiddleware.__is_path_white_list(path):
+
+        if AuthMiddleware.__not_check_login(path):
             return
 
-        # 如果用户没登录
+        # 如果用户没登录，就先重定向到登录页
         if request_api.is_login(request) is False:
             # 如果是GET请求，则重定向
             if request.method == 'GET':
                 return redirect('/login/')
             else:
                 return HttpResponse(json.dumps(dict(Result.error('请先登录！'))))
+
+        if AuthMiddleware.__not_check_auth(path):
+            return
 
         # 检查用户是否拥有执行当前action的权限
         action = request_api.get_action(request)
